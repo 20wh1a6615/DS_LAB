3b76d15b5b82d3bd299731babefd052c441b418b
20wh1a6615 <93580022+20wh1a6615@users.noreply.github.com>
2021-11-22 23:30:29 +0530

    Update SLL.c

--- Week2/SLL.c
+++ Week2/SLL.c
@@ -4,7 +4,7 @@ struct node{
      int data;
      struct node *link;
     };
- struct node *head = NULL,*cur,*temp;
+ struct node *head = NULL,*cur,*temp,*temp1;
  struct node* create(){
      int n;
      printf("enter the no. of nodes");
@@ -24,7 +24,7 @@ struct node{
              }
  }
  return head;
- 27 }
+ }
  //insertion
  struct node* insert_begin(int ele){
         cur = (struct node*)malloc(sizeof(struct node));
@@ -56,82 +56,275 @@ struct node{
           temp -> link = cur;
  return head;
  }
+//deletion
+struct node* delete_begin(struct node*head){
+        temp = head;
+        head = temp -> link;
+        printf("deleted element %d",temp -> data);
+        free(temp);
+return head;
+}
+struct node* delete_end(struct node*head){
+        temp = head;
+        while(temp != NULL){
+            temp1 = temp;
+            temp = temp -> link;
+            }
+        temp1 -> link = NULL;
+        printf("deleted element %d",temp -> data);
+        free(temp);
+return head;
+}
+struct node* delete_position(struct node*head,int pos){
+        temp = head;
+        int c = 1;
+        while(c < pos){
+            temp1 = temp;
+            temp = temp -> link;
+            c++;
+        }
+        temp1 -> link = temp -> link;
+        printf("deleted element %d",temp -> data);
+        free(temp);
+return head;
+}
 //display
  void display(struct node*head){
          temp = head;
          while(temp != NULL){
               if(temp -> link == NULL){
-                    printf("%d",temp -> data);
-                    break;
-                  }
+                   printf("%d",temp -> data);
+                      break;
+             }
                 printf("%d -> ",temp -> data);
                 temp = temp -> link;
           }
   }
+//reverse display
+void reverse_display(struct node*head){
+            if(head != NULL){
+                reverse_display(head -> link);
+                printf("%d",head -> data);
+            }
+}
+int search(struct node*head,int key){
+            int c = 1;
+            temp = head;
+            while(temp != NULL){
+                if(key == temp -> data){
+                    return c;
+                }
+                temp = temp -> link;
+                c++;
+            }
+return -1;
+}
+
+struct node* sorting(struct node*head){
+            temp = head;
+            int x;
+            while(temp != NULL){
+                temp1 = head;
+                while(temp1 != NULL){
+                    if(temp1 -> data > temp1 -> link -> data){
+                        x = temp1 -> data;
+                        temp1 -> data = temp1 -> link -> data;
+                        temp1 -> link -> data = x;
+                    }
+                    temp1 = temp1 -> link;
+                }
+                temp = temp -> link;
+            }
+}
  int main(){
-       int ele,ch,pos;
+       int ele,ch,pos,key;
        while(1){
-          printf("\n1-create\n2-insertion at begin\n3-insertion at end\n4-insertion at position");
+          printf("\n1-create\n2-insertion at begin\n3-insertion at end\n4-insertion at position\n5-delete at begin\n6-delete at end\n7-delete at position\n8-display\n9-reverse display\n10-seraching\n11-sorting\n");
           printf("enter your choice");
           scanf("%d",&ch);
           switch(ch){
               case 1:head = create();
-                    display(head);
                     break;
               case 2:printf("enter the element to be inserted");
                     scanf("%d",&ele);
                     head = insert_begin(ele);
-                    display(head);
                     break;
               case 3:printf("enter the num to be inserted at the end");
                     scanf("%d",&ele);
                     head = insert_end(ele);
-                    display(head);
                     break;
               case 4:printf("enter the position of the element to be inserted");
                     scanf("%d",&pos);
                     printf("enter the element to be inserted");
                     scanf("%d",&ele);
                     head = insert_position(pos,ele);
-                    display(head);
-              case 5:exit(0);
+                    break;
+              case 5:head = delete_begin(head);
+                     break;
+              case 6:head = delete_end(head);
+                     break;
+              case 7:printf("enter the postion of the element to be deleted");
+                     scanf("%d",&pos);
+                     head = delete_position(head,pos);
+                     break;
+              case 8:display(head);
+                     break;
+              case 9:reverse_display(head);
+                     break;
+              case 10:printf("enter the element to be searched");
+                      scanf("%d",&key);
+                      pos = search(head,key);
+                      if(pos == -1)
+                         printf("element not found");
+                      else
+                         printf("element found at position %d",pos);
+                      break;
+              case 11:head = sorting(head);
+                      break;
+              case 12:exit(0);
           }
-   }
- } 
-//ouput
+       }
+ }
+
+//output         
 1-create
 2-insertion at begin
 3-insertion at end
 4-insertion at position
+5-delete at begin
+6-delete at end
+7-delete at position
+8-display
+9-reverse display
+10-seraching
+11-sorting
 enter your choice1
-enter the no. of nodes5
-enter the data2
+enter the no. of nodes3
 enter the data3
 enter the data4
 enter the data5
-enter the data6
+
+1-create
+2-insertion at begin
+3-insertion at end
+4-insertion at position
+5-delete at begin
+6-delete at end
+7-delete at position
+8-display
+9-reverse display
+10-seraching
+11-sorting
+enter your choice8
+3 -> 4 -> 5
+1-create
+2-insertion at begin
+3-insertion at end
+4-insertion at position
+5-delete at begin
+6-delete at end
+7-delete at position
+8-display
+9-reverse display
+10-seraching
+11-sorting
+enter your choice2
+enter the element to be inserted2
+
+1-create
+2-insertion at begin
+3-insertion at end
+4-insertion at position
+5-delete at begin
+6-delete at end
+7-delete at position
+8-display
+9-reverse display
+10-seraching
+11-sorting
+enter your choice8
+2 -> 3 -> 4 -> 5
+1-create
+2-insertion at begin
+3-insertion at end
+4-insertion at position
+5-delete at begin
+6-delete at end
+7-delete at position
+8-display
+9-reverse display
+10-seraching
+11-sorting
+enter your choice3
+enter the num to be inserted at the end6
+
+1-create
+2-insertion at begin
+3-insertion at end
+4-insertion at position
+5-delete at begin
+6-delete at end
+7-delete at position
+8-display
+9-reverse display
+10-seraching
+11-sorting
+enter your choice8
 2 -> 3 -> 4 -> 5 -> 6
 1-create
 2-insertion at begin
 3-insertion at end
 4-insertion at position
-enter your choice2
-enter the element to be inserted1
-1 -> 2 -> 3 -> 4 -> 5 -> 6
-1-create
-2-insertion at begin
-3-insertion at end
-4-insertion at position
-enter your choice3
-enter the num to be inserted at the end7
-1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7
-1-create
-2-insertion at begin
-3-insertion at end
-4-insertion at position
+5-delete at begin
+6-delete at end
+7-delete at position
+8-display
+9-reverse display
+10-seraching
+11-sorting
 enter your choice4
-enter the position of the element to be inserted6
-enter the element to be inserted8
-1 -> 2 -> 3 -> 4 -> 5 -> 8 -> 6 -> 7
+enter the position of the element to be inserted3
+enter the element to be inserted50
 
-                                           
+1-create
+2-insertion at begin
+3-insertion at end
+4-insertion at position
+5-delete at begin
+6-delete at end
+7-delete at position
+8-display
+9-reverse display
+10-seraching
+11-sorting
+enter your choice8
+2 -> 3 -> 50 -> 4 -> 5 -> 6
+1-create
+2-insertion at begin
+3-insertion at end
+4-insertion at position
+5-delete at begin
+6-delete at end
+7-delete at position
+8-display
+9-reverse display
+10-seraching
+11-sorting
+enter your choice5
+deleted element 2
+1-create
+2-insertion at begin
+3-insertion at end
+4-insertion at position
+5-delete at begin
+6-delete at end
+7-delete at position
+8-display
+9-reverse display
+10-seraching
+11-sorting
+enter your choice8
+3 -> 50 -> 4 -> 5 -> 6
+
+
+
